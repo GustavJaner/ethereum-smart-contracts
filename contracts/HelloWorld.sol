5dce007f1107965992e7e270e6079b9e3a5090c8
GustavJaner <gustav.janer@gmail.com>
2019-08-04 21:27:13 +0200

    [SC] Add hello world contract

---
+++ contracts/HelloWorld.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.5.1;
+
+contract Hello {
+    string public helloState = "Hello!"; // declaring state variable as public: creates a getter func() automatically
+
+    function helloFunc() public pure returns (string memory) { // pure function: does not read/write to the state
+        return "Hello, world!";                                // for array parameters such as byte or string: 'memory' has to be explicitly be stated
+    }
+}
